28b612d40d12f07210948e0ed0faea4445922934
Tejas Nashikkar <tj.tejas77@gmail.com>
2024-05-17 20:42:29 -0400

    Week 2 - Part 2

--- public/App.js
+++ public/App.js
@@ -1,54 +1,81 @@
-// const div = (
-//     <div title="Outer Div" className="wrapper">
-//         <h1>Hello World!</h1>
-//     </div>
-// );
-
-// Class based component
-class HelloWorld extends React.Component {
-  render() {
-    const continents = ["Asia", "Africa", "Russia"];
-    const helloContinents = Array.from(continents, continent => `Hello ${continent}!`);
-    console.log("Array", helloContinents);
-    const str = helloContinents.join(" ");
-    console.log("String", str);
-    return /*#__PURE__*/React.createElement("div", {
-      title: "Outer Div",
-      className: "wrapper"
-    }, /*#__PURE__*/React.createElement("h1", null, "From String: ", str), /*#__PURE__*/React.createElement("p", null, "From Array:"), helloContinents.map((continent, indx) => /*#__PURE__*/React.createElement("h3", {
-      key: indx
-    }, continent)));
-  }
-}
-
-// Function based component
-const HelloWorldFunctn = () => {
-  const continents = ["Asia", "Africa", "Russia"];
-  const helloContinents = Array.from(continents, continent => `Hello ${continent}!`);
-  const str = helloContinents.join(" ");
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "From String: ", str), /*#__PURE__*/React.createElement("p", null, "From Array:"), helloContinents.map((continent, indx) => /*#__PURE__*/React.createElement("h3", {
-    key: indx
-  }, continent)));
-};
-
 // Child Component
-const IssueTable = ({
-  text
+const IssueRow = ({
+  style,
+  issue
 }) => {
-  console.log(text);
-  return /*#__PURE__*/React.createElement("h4", null, "This is ", text);
+  console.log("issue", issue);
+  console.log("entries", Object.entries(issue));
+  const created = issue.created !== undefined ? issue.created.toDateString() : "Date not defined";
+  const due = issue.due !== undefined ? issue.due.toDateString() : "Date not defined";
+  return /*#__PURE__*/React.createElement("tr", null, Object.entries(issue).map(value => {
+    if (value[0] === "created") {
+      return /*#__PURE__*/React.createElement("td", {
+        key: value[0],
+        style: style
+      }, created);
+    } else if (value[0] === "due") {
+      return /*#__PURE__*/React.createElement("td", {
+        key: value[0],
+        style: style
+      }, due);
+    }
+    return /*#__PURE__*/React.createElement("td", {
+      key: value[0],
+      style: style
+    }, value[1]);
+  }));
+};
+const IssueTable = ({
+  issues
+}) => {
+  // console.log("issueList", issues);
+
+  const rowStyle = {
+    border: "1px solid silver",
+    padding: 4
+  };
+  const issueRows = issues.map(issue => /*#__PURE__*/React.createElement(IssueRow, {
+    key: issue.id,
+    style: rowStyle,
+    issue: issue
+  }));
+  return /*#__PURE__*/React.createElement("table", null, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, Object.keys(issues[0]).map(title => /*#__PURE__*/React.createElement("th", {
+    key: title,
+    style: rowStyle
+  }, title)))), /*#__PURE__*/React.createElement("tbody", null, issueRows));
 };
 const IssueAdd = () => {
   return /*#__PURE__*/React.createElement("h4", null, "This is IssueAdd");
 };
 const IssuuFilter = () => {
-  return /*#__PURE__*/React.createElement("h4", null, "This is IssuuFilter");
+  return /*#__PURE__*/React.createElement("h4", {
+    style: {
+      color: "red"
+    }
+  }, "This is IssuuFilter");
 };
 
 // Parent Component
 const IssueList = () => {
+  const issueList = [{
+    id: 1,
+    status: "New",
+    owner: "Raven",
+    effort: 5,
+    created: new Date("2018-08-15"),
+    due: undefined,
+    title: "Error in console while click Add button"
+  }, {
+    id: 2,
+    status: "Assigned",
+    owner: "Eddie",
+    effort: 14,
+    created: new Date("2018-08-16"),
+    due: new Date("2018-08-30"),
+    title: "Missing border bottom on panel"
+  }];
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(IssueTable, {
-    text: "IssueTable"
+    issues: issueList
   }), /*#__PURE__*/React.createElement(IssueAdd, null), /*#__PURE__*/React.createElement(IssuuFilter, null));
 };
 const element = /*#__PURE__*/React.createElement(IssueList, null);
--- src/App.js
+++ src/App.js
@@ -1,55 +1,77 @@
-// const div = (
-//     <div title="Outer Div" className="wrapper">
-//         <h1>Hello World!</h1>
-//     </div>
-// );
+// Child Component
+const IssueRow = ({ style, issue }) => {
+    console.log("issue", issue);
+    console.log("entries", Object.entries(issue));
+    const created =
+        issue.created !== undefined
+            ? issue.created.toDateString()
+            : "Date not defined";
 
-// Class based component
-class HelloWorld extends React.Component {
-    render() {
-        const continents = ["Asia", "Africa", "Russia"];
-        const helloContinents = Array.from(
-            continents,
-            (continent) => `Hello ${continent}!`
-        );
-        console.log("Array", helloContinents);
-        const str = helloContinents.join(" ");
-        console.log("String", str);
-        return (
-            <div title="Outer Div" className="wrapper">
-                <h1>From String: {str}</h1>
-                <p>From Array:</p>
-                {helloContinents.map((continent, indx) => (
-                    <h3 key={indx}>{continent}</h3>
-                ))}
-            </div>
-        );
-    }
-}
-
-// Function based component
-const HelloWorldFunctn = () => {
-    const continents = ["Asia", "Africa", "Russia"];
-    const helloContinents = Array.from(
-        continents,
-        (continent) => `Hello ${continent}!`
-    );
-    const str = helloContinents.join(" ");
+    const due =
+        issue.due !== undefined ? issue.due.toDateString() : "Date not defined";
     return (
-        <>
-            <h1>From String: {str}</h1>
-            <p>From Array:</p>
-            {helloContinents.map((continent, indx) => (
-                <h3 key={indx}>{continent}</h3>
-            ))}
-        </>
+        <tr>
+            {Object.entries(issue).map((value) => {
+                if (value[0] === "created") {
+                    return (
+                        <td key={value[0]} style={style}>
+                            {created}
+                        </td>
+                    );
+                } else if (value[0] === "due") {
+                    return (
+                        <td key={value[0]} style={style}>
+                            {due}
+                        </td>
+                    );
+                }
+                return (
+                    <td key={value[0]} style={style}>
+                        {value[1]}
+                    </td>
+                );
+            })}
+            {/* <td style={style}>{issue.id}</td>
+            <td style={style}>{issue.status}</td>
+            <td style={style}>{issue.owner}</td>
+            <td style={style}>{issue.effort}</td>
+            <td style={style}>{created}</td>
+            <td style={style}>{due}</td>
+            <td style={style}>{issue.title}</td> */}
+        </tr>
     );
 };
 
-// Child Component
-const IssueTable = ({ text }) => {
-    console.log(text);
-    return <h4>This is {text}</h4>;
+const IssueTable = ({ issues }) => {
+    // console.log("issueList", issues);
+
+    const rowStyle = { border: "1px solid silver", padding: 4 };
+
+    const issueRows = issues.map((issue) => (
+        <IssueRow key={issue.id} style={rowStyle} issue={issue} />
+    ));
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    {Object.keys(issues[0]).map((title) => (
+                        <th key={title} style={rowStyle}>
+                            {title}
+                        </th>
+                    ))}
+                    {/* <th style={rowStyle}>ID</th>
+                    <th style={rowStyle}>Status</th>
+                    <th style={rowStyle}>Owner</th>
+                    <th style={rowStyle}>Effort</th>
+                    <th style={rowStyle}>Created</th>
+                    <th style={rowStyle}>Due</th>
+                    <th style={rowStyle}>Title</th> */}
+                </tr>
+            </thead>
+            <tbody>{issueRows}</tbody>
+        </table>
+    );
 };
 
 const IssueAdd = () => {
@@ -57,14 +79,35 @@ const IssueAdd = () => {
 };
 
 const IssuuFilter = () => {
-    return <h4>This is IssuuFilter</h4>;
+    return <h4 style={{ color: "red" }}>This is IssuuFilter</h4>;
 };
 
 // Parent Component
 const IssueList = () => {
+    const issueList = [
+        {
+            id: 1,
+            status: "New",
+            owner: "Raven",
+            effort: 5,
+            created: new Date("2018-08-15"),
+            due: undefined,
+            title: "Error in console while click Add button",
+        },
+        {
+            id: 2,
+            status: "Assigned",
+            owner: "Eddie",
+            effort: 14,
+            created: new Date("2018-08-16"),
+            due: new Date("2018-08-30"),
+            title: "Missing border bottom on panel",
+        },
+    ];
+
     return (
         <>
-            <IssueTable text={"IssueTable"} />
+            <IssueTable issues={issueList} />
             <IssueAdd />
             <IssuuFilter />
         </>
